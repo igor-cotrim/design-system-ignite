3cf145a9071e841d03d2a22fc0d4a15b0a66c71b
Igor Cotrim <igor.santos@verity.com.br>
2022-10-19 10:40:12 -0300

    create component Avatar

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,18 @@
+import { ComponentProps } from 'react'
+import { User } from 'phosphor-react'
+
+import * as S from './styles'
+
+export interface AvatarProps extends ComponentProps<typeof S.AvatarImage> {}
+
+export const Avatar = (props: AvatarProps) => {
+  return (
+    <S.AvatarContainer>
+      <S.AvatarImage {...props} />
+
+      <S.AvatarFallback delayMs={600}>
+        <User />
+      </S.AvatarFallback>
+    </S.AvatarContainer>
+  )
+}
